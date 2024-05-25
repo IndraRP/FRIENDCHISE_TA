1988a2cedd0fce020f565fdec929c7e78a95f13c
IndraRP <indraridho08@gmail.com>
2024-05-25 12:31:59 +0700

    Penghapusan Login Admin, Navbar Admin dan Read Admin

--- src/pages/Login_admin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login_admin = () => {
-  return (
-    <div>Login_admin</div>
-  )
-}
-
-export default Login_admin
--- src/pages/Read_admin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Read_admin = () => {
-  return (
-    <div>Read_admin</div>
-  )
-}
-
-export default Read_admin
